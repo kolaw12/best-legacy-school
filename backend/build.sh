c31ead48557cab291606cb8dcfe63d88129a9761
opeoluwa <kolawolemoses50@gmail.com>
2026-01-17 15:11:16 +0100

    Fix build script to ensure database tables are created

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations 
+python manage.py makemigrations core
 python manage.py migrate
